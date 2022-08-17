0c6f64b16e7a50c6d71cc1d7ab86869fad2ab140
larrysfinolb <larrysfinolb@gmail.com>
2022-08-17 18:16:10 -0400

    Updates

--- src/utils/getHash.js
+++
@@ -1,5 +0,0 @@
-const getHash = () => {
-	return location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/';
-};
-
-export default getHash;
--- src/utils/resolveRoutes.js
+++
@@ -1,6 +0,0 @@
-const resolveRoutes = (route) => {
-	if (route === 'login') return '/login';
-	return route === '/' ? '/' : `/${route}`;
-};
-
-export default resolveRoutes;
